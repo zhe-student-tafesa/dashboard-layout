d5e76599ba6c15a2db9b00a584d0ad31e279ba85
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-10-22 12:34:00 +1000

    1. ues map show items;

--- src/components/sideBar/subMenu/SubMenu.tsx
+++ src/components/sideBar/subMenu/SubMenu.tsx
@@ -2,22 +2,44 @@
 import React, { useState } from 'react'
 import styles from './subMenu.module.css'
 import { SidebarItem } from '../subBarData';
+import Link from 'next/link';
 
-
-  
-  interface SubMenuProps {
-    item: SidebarItem;
-  }
-
-  
+interface SubMenuProps {
+  item: SidebarItem;
+}
 
 const SubMenu: React.FC<SubMenuProps> = ({ item }) => {
-    const [subnav, setSubnav] = useState(false);
+  const [subnav, setSubnav] = useState(false);
 
-    const showSubnav = () => setSubnav(!subnav);
-    return (
-        <div>SubMenu</div>
-    )
+  const showSubnav = () => setSubnav(!subnav);
+  return (
+    <>
+      <Link href={item.path} passHref>
+        <div className={styles.sidebarLink} onClick={item.subNav ? showSubnav : undefined}>
+          <div>
+            {item.icon && <item.icon />} {/* make sure icon exist */}
+            <span className={styles.sidebarLabel}>{item.title}</span>
+          </div>
+          <div>
+            {item.subNav && subnav
+              ? item.iconOpened && <item.iconOpened />
+              : item.subNav
+                ? item.iconClosed && <item.iconClosed />
+                : null}
+          </div>
+        </div>
+      </Link>
+      {subnav &&
+        item.subNav?.map((subItem, index) => (
+          <Link href={subItem.path} key={index} passHref>
+            <div className={styles.dropdownLink}>
+              {subItem.icon && <subItem.icon />} {/* make sure icon exist */}
+              <span className={styles.sidebarLabel}>{subItem.title}</span>
+            </div>
+          </Link>
+        ))}
+    </>
+  );
 }
 
 export default SubMenu
